5517ee57cb9ab4a310ee50f06afdddbea7ff6e02
Rioto Oka <okarioto@gmail.com>
2025-07-04 15:36:32 -0700

    add _redirects, fix socials

--- client/src/components/Socials.jsx
+++ client/src/components/Socials.jsx
@@ -1,12 +1,13 @@
-
+import ig from "../src/assets/instagram.svg"
+import fb from "../src/assets/facebook.svg"
 export default function Socials() {
   return (
     <div className="flex w-[60%] justify-between mb-6">
       <a href="https://www.instagram.com/ubc_bc/" target="blank">
-        <img src="../src/assets/instagram.svg" alt="" className="w-7" />
+        <img src={ig} alt="" className="w-7" />
       </a>
       <a href="https://www.facebook.com/ubcbc" target="blank">
-        <img src="../src/assets/facebook.svg" alt="" className="w-7" />
+        <img src={fb} alt="" className="w-7" />
       </a>
     </div>
   );
